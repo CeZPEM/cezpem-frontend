61315cee443925f60259846b7622a8e59ea5d9d9
Richard A <odeiospam2012@live.com>
2025-02-08 18:39:02 -0300

    fix: update header to use Link component for logo navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,16 @@ import { IconStar } from "./Icons";
 import Navbar from "./Navbar";
 import { CezpemLogoText } from "./Logos";
 import Container from "./Container";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-white shadow-md">
       <Container className="!py-0">
         <div className="mx-auto flex flex-col md:flex-row gap-5 justify-between items-center py-3">
-          <div className="flex items-center justify-center rounded">
+          <Link href="/" className="flex items-center justify-center">
             <CezpemLogoText />
-          </div>
+          </Link>
 
           <Navbar />
 
