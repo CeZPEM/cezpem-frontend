3b9b0b89bfa6f8225f518c9c2223f0a6ec371d8b
Richard A <odeiospam2012@live.com>
2025-02-09 15:49:31 -0300

    feat: add Cursos component for course listing page

---
+++ src/app/cursos/page.tsx
@@ -0,0 +1,7 @@
+export default function Cursos() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-3xl font-bold">Cursos</h1>
+    </div>
+  );
+}
