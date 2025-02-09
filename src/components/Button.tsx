7bed095e71d5707a32a5d2556d7e2bd65700de10
Richard A <odeiospam2012@live.com>
2025-02-08 21:04:48 -0300

    fix: remove unnecessary border width from Button component for cleaner styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,17 @@ import { ButtonHTMLAttributes } from "react";
 export function Button({
   children,
   className,
+
   ...props
 }: {
   children: React.ReactNode;
   className?: string;
+
   props?: ButtonHTMLAttributes<HTMLButtonElement>;
 }) {
   return (
     <button
-      className={`bg-red text-white px-4 py-2 rounded-lg flex items-center border-2 border-black hover:opacity-80 transition ${className}`}
+      className={`bg-red text-white px-4 py-2 rounded-lg flex items-center border border-black hover:opacity-80 transition ${className}`}
       {...props}
     >
       {children}
