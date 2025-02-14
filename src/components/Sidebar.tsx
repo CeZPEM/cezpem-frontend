f2bee9c90e20e2697e22c87d8b2b15e0f54146d5
Richard A <odeiospam2012@live.com>
2025-02-14 20:48:25 -0300

    feat: enhance Sidebar component with dynamic active link highlighting and logo update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,9 @@
 
 import { menuItems } from "@/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FiArrowLeft } from "react-icons/fi";
+import { CezpemLogoText } from "./Logos";
 
 type SidebarProps = {
   isOpen: boolean;
@@ -10,15 +12,19 @@ type SidebarProps = {
 };
 
 export default function Sidebar({ isOpen, setIsOpen }: SidebarProps) {
+  const pathname = usePathname();
+
   return (
     <aside
-      className={`fixed top-0 left-0 h-full w-64 bg-white shadow transition-transform duration-300 z-30 transform ${
+      className={`fixed top-0 left-0 h-full w-72 bg-white shadow transition-transform duration-300 z-30 transform ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       }`}
     >
       {/* Header with Logo and Toggle */}
       <div className="flex items-center justify-between p-4">
-        <div className="flex items-center gap-3">Logo CeZPEM</div>
+        <div className="flex items-center gap-3">
+          <CezpemLogoText className="w-36" />
+        </div>
         <button onClick={() => setIsOpen(false)} className="p-2">
           <FiArrowLeft size={24} />
         </button>
@@ -32,7 +38,9 @@ export default function Sidebar({ isOpen, setIsOpen }: SidebarProps) {
               <li key={item.label}>
                 <Link
                   href={item.href}
-                  className="block p-2 rounded hover:bg-gray-200"
+                  className={`block p-2 rounded hover:bg-gray-200 ${
+                    pathname === item.href ? "bg-gray-200" : ""
+                  }`}
                   onClick={() => setIsOpen(false)}
                 >
                   {item.label}
