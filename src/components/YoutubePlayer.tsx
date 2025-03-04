f53a9e415bf07ffbf29baf62c8fc120fec69bba8
Richard A <odeiospam2012@live.com>
2025-03-04 13:43:08 -0300

    feat: add new image assets and update menu items for improved navigation

--- src/components/YoutubePlayer.tsx
+++ src/components/YoutubePlayer.tsx
@@ -1,11 +1,9 @@
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import { RefAttributes } from "react";
+import LiteYouTubeEmbed, { LiteYouTubeProps } from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 export default function YoutubePlayer({
   ...props
-}: {
-  id: string;
-  title: string;
-}) {
+}: LiteYouTubeProps & RefAttributes<HTMLIFrameElement>) {
   return <LiteYouTubeEmbed {...props} />;
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const menuItems = [
-  { released: true, label: "Home", href: "/" },
+  { released: true, label: "Início", href: "/" },
   { released: false, label: "Cursos", href: "/cursos" },
   { released: true, label: "Cursinho Popular", href: "/cursinho" },
   { released: true, label: "Sobre", href: "/sobre" },
