ca57cfc43c1726574a72a5cfe075309633f06e54
Richard A <odeiospam2012@live.com>
2025-03-07 22:43:09 -0300

    feat: update layout and responsiveness of Home page sections for improved design and accessibility

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
     <Page>
       <Section
         id="inicio"
-        className="relative flex h-screen sm:h-[720px] bg-navyBlue"
+        className="relative flex h-screen xs:h-[720px] bg-navyBlue"
       >
         <div className="w-full h-full absolute hidden md:block top-0 left-0 z-0 overflow-hidden">
           <Image
@@ -52,7 +52,7 @@ export default function Home() {
             <h1 className="text-40px sm:text-64px font-bold">
               Conheça o{" "}
               <span className="sm:-ml-2 h-7 md:h-16 w-[150px] sm:w-[249px] inline-block">
-                <span className="relative flex w-auto h-auto sm:h-[94px] mt-4 sm:mt-0">
+                <span className="relative flex w-auto h-auto sm:h-[24px] md:h-[94px] mt-4 sm:mt-0">
                   <Image
                     alt="Logo do CeZPEM"
                     src="/CeZPEM.svg"
@@ -95,10 +95,10 @@ export default function Home() {
 
       <Section
         id="cursinho"
-        className="h-screen sm:h-auto md:h-[660px] flex items-end"
+        className="h-screen xs:h-auto md:h-[660px] flex items-end"
       >
         <Container className="mt-8 md:mt-auto md:mb-0 !py-0 !px-0 md:px-4 overflow-hidden">
-          <div className="flex flex-col-reverse md:flex-row items-end gap-x-8">
+          <div className="flex flex-col-reverse lg:flex-col-reverse xl:flex-row items-end gap-x-8">
             <div className="relative w-full md:w-full min-h-[200px] md:min-w-96 flex md:flex-1 flex-1 right-0 overflow-hidden md:overflow-visible">
               <Image
                 alt="CeZPEM"
@@ -109,7 +109,7 @@ export default function Home() {
               />
             </div>
 
-            <div className="md:w-[34%] flex flex-col gap-3 md:gap-0 md:max-w-[460px] px-4 md:px-0">
+            <div className="lg:w-[34%] mx-auto flex flex-col gap-3 md:gap-0 md:max-w-[460px] px-4 md:px-0">
               <Image
                 alt="CeZPEM"
                 src="/CeZPEM-bottom.png"
@@ -118,25 +118,25 @@ export default function Home() {
                 height={89}
               />
 
-              <PageTitle className="relative text-center md:text-right">
+              <PageTitle className="relative text-center lg:text-right">
                 <span className="relative z-10">
                   Cursinho Popular Pré-Vestibular
                 </span>
                 <Image
                   alt="CeZPEM"
                   src="/details-text.png"
-                  className="absolute z-0 m-auto w-auto max-w-full transform -translate-y-1/2 top-7 -right-1 md:top-11 md:-right-11"
+                  className="absolute z-0 m-auto w-auto max-w-full transform -translate-y-1/2 top-7 -right-1 md:top-11 lg:-right-11"
                   width={484 * 2}
                   height={28 * 2}
                 />
               </PageTitle>
 
-              <p className="w-full leading-tight font-archivo text-20px md:text-24px md:mt-6 text-center md:text-right">
+              <p className="w-full leading-tight font-archivo text-20px md:text-24px md:mt-6 text-center lg:text-right">
                 O cursinho Popular do <b> CeZPEM </b> oferece aulas gratuitas
                 voltadas aos conteúdos da grade regular do Enem.
               </p>
 
-              <p className="w-full leading-tight md:my-6 font-archivo text-14px md:text-16px text-center md:text-right">
+              <p className="w-full leading-tight md:my-6 font-archivo text-14px md:text-16px text-center lg:text-right">
                 As aulas ocorrem ao vivo, para que adolescentes e adultos tenham
                 acesso ao conteúdo de um cursinho preparatório, e ainda possam
                 tirar suas dúvidas de forma direta com os professores.
@@ -160,7 +160,7 @@ export default function Home() {
 
       <Section
         id="apoie"
-        className="relative h-screen sm:h-[720px] bg-red grid grid-cols-1"
+        className="relative h-screen sm:h-[1024px] xl:h-[720px] bg-red grid grid-cols-1"
       >
         <div className="w-full h-full absolute hidden md:block top-0 left-0 z-0 overflow-hidden">
           <Image
@@ -197,7 +197,7 @@ export default function Home() {
           />
         </div>
 
-        <Container className="relative z-10 sm:h-[720px] grid grid-cols-1 md:grid-cols-[36%_1fr] gap-8 md:gap-0 !px-0">
+        <Container className="relative z-10 xl:h-[720px] grid grid-cols-1 xl:grid-cols-[36%_1fr] gap-8 md:gap-0 px-0">
           <div className="w-full h-auto my-auto flex flex-col gap-4 text-white">
             <PageTitle className="relative text-center w-auto mx-auto mt-20 md:mt-0">
               <span className="relative z-10">Apoie Nosso Projeto!</span>
@@ -246,7 +246,7 @@ export default function Home() {
       </Section>
 
       <Section id="sobre" className="min-h-screen h-auto sm:min-h-[720px] grid">
-        <Container className="w-full h-full pt-28 sm:pt-44 md:pt-12 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-12">
+        <Container className="w-full h-full pt-28 sm:pt-44 md:pt-12 grid grid-cols-1 xl:grid-cols-[1fr_1fr] gap-12">
           <div className="order-last md:order-none w-full h-full flex flex-col items-center justify-center">
             <Image
               alt="CeZPEM"
