344d588024c98c38b60faeb2d2264e5354fa9530
Richard A <odeiospam2012@live.com>
2025-03-13 20:43:17 -0300

    feat: create Section1 component to encapsulate homepage content and animations

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,15 +3,11 @@
 import { Button } from "@/components/Button";
 import Container from "@/components/Container";
 import HotSiteFAQ from "@/components/HotSiteFAQ";
-import { SoberanaLogoText } from "@/components/Logos";
 import Page from "@/components/Page";
 import Section from "@/components/Section";
-import YoutubePlayer from "@/components/YoutubePlayer";
-import Lottie from "lottie-react";
 import Image from "next/image";
 import Link from "next/link";
-
-import highlightAnimation from "../../../public/highlight.json";
+import Section1 from "./components/Section1";
 
 type PageTitleProps = {
   children: React.ReactNode;
@@ -33,94 +29,7 @@ function PageTitle({ children, className }: PageTitleProps) {
 export default function Home() {
   return (
     <Page>
-      <Section
-        id="inicio"
-        className="relative flex pt-10 md:pt-0 h-auto md:min-h-[846px] bg-navyBlue"
-      >
-        <div className="w-full h-full absolute hidden md:block top-0 left-0 z-0 overflow-hidden">
-          <Image
-            priority={false}
-            loading="lazy"
-            alt="CeZPEM"
-            src="/details-1-top-right.png"
-            className="absolute -top-8 -right-[1px]"
-            width={249}
-            height={89}
-          />
-
-          <Image
-            priority={false}
-            loading="lazy"
-            alt="CeZPEM"
-            src="/details-1-bottom-left.png"
-            className="absolute -bottom-[1px] -left-[1px]"
-            width={249}
-            height={89}
-          />
-        </div>
-
-        <Container className="relative my-auto !px-2 md:px-4 pb-10 z-10">
-          <div className="w-full text-white text-center flex flex-col items-center justify-center gap-2 px-4 sm:px-0">
-            <h1 className="text-40px sm:text-64px font-bold">
-              Conheça o{" "}
-              <span className="sm:-ml-2 h-7 md:h-16 w-[150px] sm:w-[249px] inline-block">
-                <span className="relative flex w-auto h-auto sm:h-[24px] md:h-[94px] mt-4 sm:mt-0">
-                  <Image
-                    priority
-                    loading="eager"
-                    alt="Logo do CeZPEM"
-                    src="/CeZPEM.svg"
-                    className="absolute z-[1] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-auto"
-                    width={249 * 2}
-                    height={89 * 2}
-                  />
-
-                  <span className="absolute z-[0] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[180px] h-[74px] sm:w-[264px] sm:h-[98px]">
-                    <Lottie animationData={highlightAnimation} loop={false} />
-                  </span>
-                </span>
-              </span>
-            </h1>
-
-            <div className="font-archivo mx-auto max-w-[700px] text-20px md:text-24px">
-              O Centro <b> Zoia Prestes </b> de Educação Multidisciplinar
-              (CeZPEM) é uma frente de massas do Coletivo{" "}
-              <span className="ml-2 h-6 w-32 inline-block">
-                <span className="relative flex h-5 mt-0">
-                  <SoberanaLogoText
-                    size="small"
-                    variant="white"
-                    className="w-32 absolute"
-                  />
-                </span>
-              </span>
-            </div>
-          </div>
-
-          <div className="mx-auto w-full max-w-[960px] mt-14 md:mt-8 rounded-md overflow-hidden">
-            <div className="h-full w-full max-h-screen max-w-[100vw] hidden md:block">
-              <YoutubePlayer
-                thumbnail="/video-cover.jpg"
-                id="kRQkAR05kAI"
-                title="Organize-se no CeZPEM"
-                aspectHeight={9}
-                aspectWidth={16}
-              />
-            </div>
-
-            <div className="h-full w-full max-h-[98vh] max-w-[100vw] md:hidden [&>article]:max-h-[94vh] md:[&>article]:max-h-none">
-              <YoutubePlayer
-                thumbnail="/video-cover.jpg"
-                id="fIo4NwZXIHU"
-                title="Organize-se no CeZPEM"
-                aspectHeight={16}
-                aspectWidth={9}
-                activatedClass="w-[100vw] max-h-[98vh] w-full h-full [&>button]:hidden"
-              />
-            </div>
-          </div>
-        </Container>
-      </Section>
+      <Section1 />
 
       <Section
         id="cursinho"
