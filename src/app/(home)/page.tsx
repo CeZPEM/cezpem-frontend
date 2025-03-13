a9fd43ae29f41f029aecb7ab452f1eb88af919aa
Richard A <odeiospam2012@live.com>
2025-03-13 20:25:12 -0300

    fix: optimize image loading by setting priority and loading attributes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -39,6 +39,8 @@ export default function Home() {
       >
         <div className="w-full h-full absolute hidden md:block top-0 left-0 z-0 overflow-hidden">
           <Image
+            priority={false}
+            loading="lazy"
             alt="CeZPEM"
             src="/details-1-top-right.png"
             className="absolute -top-8 -right-[1px]"
@@ -47,6 +49,8 @@ export default function Home() {
           />
 
           <Image
+            priority={false}
+            loading="lazy"
             alt="CeZPEM"
             src="/details-1-bottom-left.png"
             className="absolute -bottom-[1px] -left-[1px]"
@@ -62,6 +66,8 @@ export default function Home() {
               <span className="sm:-ml-2 h-7 md:h-16 w-[150px] sm:w-[249px] inline-block">
                 <span className="relative flex w-auto h-auto sm:h-[24px] md:h-[94px] mt-4 sm:mt-0">
                   <Image
+                    priority
+                    loading="eager"
                     alt="Logo do CeZPEM"
                     src="/CeZPEM.svg"
                     className="absolute z-[1] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-auto"
@@ -124,6 +130,8 @@ export default function Home() {
           <div className="flex flex-col-reverse xl:flex-row items-center md:items-end justify-center lg:gap-x-8">
             <div className="relative w-full md:w-full mx-auto min-h-[200px] md:min-w-96 flex flex-1 right-0 overflow-hidden md:overflow-visible">
               <Image
+                priority={false}
+                loading="lazy"
                 alt="CeZPEM"
                 src="/enem-cezpem.png"
                 className="max-w-none absolute md:relative w-[120vw] md:w-full md:max-w-[926px] -left-8 top-4 mx-auto md:left-auto xl:left-12 md:top-0"
@@ -134,6 +142,8 @@ export default function Home() {
 
             <div className="xl:w-[34%] mx-auto flex flex-col gap-3 lg:gap-0 sm:mt-8 md:mt-16 md:max-w-[720px] px-4 md:px-0">
               <Image
+                priority={false}
+                loading="lazy"
                 alt="CeZPEM"
                 src="/CeZPEM-bottom.png"
                 className="w-full md:hidden max-w-[100px] mx-auto"
@@ -146,6 +156,8 @@ export default function Home() {
                   Cursinho Popular Pré-Vestibular
                 </span>
                 <Image
+                  priority={false}
+                  loading="lazy"
                   alt="CeZPEM"
                   src="/details-text.png"
                   className="absolute z-0 w-auto max-w-full left-1/2 ml-8 top-7 md:top-12 lg:top-12 lg:-right-11 transform -translate-y-1/2 -translate-x-1/2"
@@ -187,6 +199,8 @@ export default function Home() {
       >
         <div className="w-full h-full absolute hidden md:block top-0 left-0 z-0 overflow-hidden">
           <Image
+            priority={false}
+            loading="lazy"
             alt="CeZPEM"
             src="/details-2-top-left.png"
             className="absolute -top-[1px] -left-[1px]"
@@ -195,6 +209,8 @@ export default function Home() {
           />
 
           <Image
+            priority={false}
+            loading="lazy"
             alt="CeZPEM"
             src="/details-2-bottom-right.png"
             className="absolute -bottom-8 -right-[1px]"
@@ -205,13 +221,18 @@ export default function Home() {
 
         <div className="w-full h-full absolute top-0 left-0 z-0 overflow-hidden md:overflow-visible">
           <Image
+            priority={false}
+            loading="lazy"
             alt="CeZPEM"
             src="/piggy-bank-right.png"
             className="absolute -top-1 md:-top-12 md:right-auto left-1/3 md:left-1/4 w-32 blur-[1px]"
             width={249}
             height={89}
           />
+
           <Image
+            priority={false}
+            loading="lazy"
             alt="CeZPEM"
             src="/piggy-bank-left.png"
             className="absolute bottom-[40%] left-auto -right-5 md:-bottom-8 md:right-auto md:left-1/3 w-24 md:w-32"
@@ -258,6 +279,8 @@ export default function Home() {
 
           <div className="w-full h-auto my-auto">
             <Image
+              priority={false}
+              loading="lazy"
               alt="CeZPEM"
               src="/piggy-bank-center.png"
               className="m-auto w-auto relative -left-8 md:-left-16 -bottom-6"
@@ -272,6 +295,8 @@ export default function Home() {
         <Container className="w-full h-full pt-28 sm:pt-44 md:pt-12 grid grid-cols-1 xl:grid-cols-[1fr_1fr] gap-12">
           <div className="order-last md:order-none w-full h-full flex flex-col items-center justify-center">
             <Image
+              priority={false}
+              loading="lazy"
               alt="CeZPEM"
               src="/CeZPEM-colagem.png"
               className="w-auto m-auto"
@@ -284,6 +309,8 @@ export default function Home() {
             <PageTitle className="w-full max-w-none relative text-center">
               <span className="relative z-10">Perguntas Frequentes</span>
               <Image
+                priority={false}
+                loading="lazy"
                 alt="CeZPEM"
                 src="/details-text.png"
                 className="absolute h-1/2 z-0 w-auto transform -translate-x-1/2 top-5 md:top-8 left-1/2 ml-3"
