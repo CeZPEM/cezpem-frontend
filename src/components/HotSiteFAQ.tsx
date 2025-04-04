7e9916d95da3a3e4b68b43d5b498a8bb4596a1cf
Richard A <richard.arias@msn.com>
2025-04-04 15:23:07 -0300

    refactor: update HotSiteFAQ component to fetch FAQs from the new API route

--- src/components/HotSiteFAQ.tsx
+++ src/components/HotSiteFAQ.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import { FaqResponse } from "@/types";
 import { FaqToggle } from "./FaqToggle";
+import FaqService from "@/services/faqService";
 
 export default function HotSiteFAQ() {
   const [faqArray, setFaqArray] = useState<FaqResponse["data"] | null>(null);
@@ -9,8 +10,15 @@ export default function HotSiteFAQ() {
 
   const FetchFaqs = async () => {
     // for use with Nextjs API
-    const response = await fetch("/api/faqs");
-    const { data } = await response.json();
+    // const response = await fetch("/api/faqs");
+    // const { data } = await response.json();
+    const { data } = await FaqService.getFaqs(
+      {
+        "filters[area][name][$eq]": "Sobre",
+        populate: "area",
+      },
+      "order:asc"
+    );
 
     setFaqArray(data);
   };
