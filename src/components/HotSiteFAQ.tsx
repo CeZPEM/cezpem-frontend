db2ccb111ab1cbbebd45d1f89c7b050ee2189e51
Richard A <richard.arias@msn.com>
2025-04-04 15:07:37 -0300

    refactor: update HotSiteFAQ component to use FaqService for fetching FAQs

--- src/components/HotSiteFAQ.tsx
+++ src/components/HotSiteFAQ.tsx
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 
 import { FaqResponse } from "@/types";
 import { FaqToggle } from "./FaqToggle";
+import FaqService from "@/services/faqService";
 
 export default function HotSiteFAQ() {
   const [faqArray, setFaqArray] = useState<FaqResponse["data"] | null>(null);
   const [openFaqIndex, setOpenFaqIndex] = useState<number | null>(null);
 
   const FetchFaqs = async () => {
-    const response = await fetch("/api/faqs");
-
-    const { data } = await response.json();
+    // for use with Nextjs API
+    // const response = await fetch("/api/faqs");
+    // const { data } = await response.json();
+    const { data } = await FaqService.getFaqs(
+      {
+        "filters[area][name][$eq]": "Sobre",
+        populate: "area",
+      },
+      "order:asc"
+    );
 
     setFaqArray(data);
   };
