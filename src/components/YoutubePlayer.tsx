467559556e1f83654955ec01cdbf13fde5d974ba
Thundree <49849865_46848166601_thundree@users.noreply.github.com>
2026-01-21 12:11:55 -0300

    refactor: implement dynamic imports for Lottie and LiteYouTubeEmbed components

--- src/components/YoutubePlayer.tsx
+++ src/components/YoutubePlayer.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { RefAttributes } from "react";
-import LiteYouTubeEmbed, { LiteYouTubeProps } from "react-lite-youtube-embed";
+import dynamic from "next/dynamic";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+const LiteYouTubeEmbed = dynamic(() => import("react-lite-youtube-embed"), {
+  ssr: false,
+});
 
 export default function YoutubePlayer({
   ...props
